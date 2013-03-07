00fb1ef6e961f48e0df1671092b0b2a1a520d8bd
yota morimoto <yotamorimoto@gmail.com>
2013-03-07 18:09:46 +0100

    update cmakelists

---
+++ src/CMakeFiles/NLUGens.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yota/Library/Application Support/SuperCollider/Extensions/nl4sc/src/NLUGens.cpp" "/Users/yota/Library/Application Support/SuperCollider/Extensions/nl4sc/src/CMakeFiles/NLUGens.dir/NLUGens.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/yota/Deployment/supercollider/include/plugin_interface"
+  "/Users/yota/Deployment/supercollider/include/common"
+  "/Users/yota/Deployment/supercollider/external_libraries/libsndfile"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ src/CMakeFiles/NLUGens.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/NLUGens.dir/NLUGens.cpp.o"
+  "NLUGens.pdb"
+  "NLUGens.scx"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/NLUGens.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
