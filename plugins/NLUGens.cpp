037c87d16f32685bf36ebf3bbf3f140bf30faf1f
yota morimoto <yotamorimoto@gmail.com>
2013-03-14 21:56:58 +0100

    src

--- plugins/NLUGens.cpp
+++ plugins/NLUGens.cpp
@@ -1,7 +1,5 @@
 /*
- SuperCollider real time audio synthesis system
- Copyright (c) 2002 James McCartney. All rights reserved.
- http://www.audiosynth.com
+ NLUGens by yota morimoto (http://yota.tehis.net/)
  
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -17,8 +15,6 @@
  along with this program; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
-
-// NLUGens by yota morimoto (http://yota.tehis.net/)
 
 #include "SC_PlugIn.h"
 
